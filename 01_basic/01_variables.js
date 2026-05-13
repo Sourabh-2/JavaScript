157a844c69e41904be87073c15c27e1b26dec48c
Sourabh-2 <128023771+Sourabh-2@users.noreply.github.com>
2026-05-13 11:35:38 +0000

    #Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const name = "Sourabh"  //It cannot be changed.
+let email = "abc@gmail.com"
+var password = "12345"
+city = "Rameshwaram"
+let state;   //It will print the undefined value, becuase we didn't give any value in it yet.
+
+
+// name = "Singh";   //Not allowed
+email = "xyz@gmai.com"
+password = "62366";
+city  = "Bengluru";
+
+console.log(name);
+
+/*
+Prefer not to use var, because of issue in block scope and functional scope.
+*/
+
+console.log([name, email, password, city, state]);
+
