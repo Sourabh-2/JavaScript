99b1b141b716b2909764bd8e4b281083a411f6c9
Sourabh-2 <128023771+Sourabh-2@users.noreply.github.com>
2026-05-14 12:16:32 +0000

    #02_datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";   //treat all JS code as newer JS version
+
+//alert(3 + 3)  // we are using nodejs, not browser. So if we will use this, this will give us error.
+
+//Datatypes:
+let name = "Sourabh"
+let age = 25
+let isloggedIn = true
+
+//Premitive Datatypes
+
+// number ==> 2 to the power 52 or 53
+// bigint
+// string = ""
+// boolean ==> true/false
+// null ==> Standalone value
+// undefined ==>
+// symbol ==> used for unique
+
+//Object
+console.log(typeof null)  //output ==> object
+console.log(typeof undefined)  //output ==> undefined
