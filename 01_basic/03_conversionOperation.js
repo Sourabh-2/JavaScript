cb0f330349ebb4d6a9d03dce416ad93b68931d4f
Sourabh-2 <128023771+Sourabh-2@users.noreply.github.com>
2026-05-18 10:50:35 +0000

    #conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// let number = "33abc"
+// let valueInNumber1 = Number(number)
+// console.log(valueInNumber1);
+
+//"33" ==> 33
+//"33abc" ==> NaN
+//true ==> 1;
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1 ==> true; 0 ==> false
+"" ==> false
+"Sourabh" ==> true
+ */
+
+let a = 33
+let stringNumber = String(a)
+console.log(stringNumber);
+console.log(typeof stringNumber);
