5efd4b816a11d7f8046319f455e65f936d7fe5af
Sourabh-2 <128023771+Sourabh-2@users.noreply.github.com>
2026-05-25 11:22:57 +0000

    #ConversionOperation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -16,7 +16,7 @@
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 1 ==> true; 0 ==> false
@@ -24,7 +24,24 @@ console.log(booleanIsLoggedIn);
 "Sourabh" ==> true
  */
 
-let a = 33
+let a = 33   
 let stringNumber = String(a)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*************************************************** Operations *************************************************
+let value = 3
+//console.log(-value)
+
+let str1 = "Hello"
+let str2 = " Sourabh"
+let str3 = str1 + str2
+// console.log(str3)
+//console.log(2**2)   // It will work like power of 2
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" +2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(+true)  //not good way for conversion
+// console.log(+"")  //not good way for conversion
